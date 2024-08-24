4ac9c2827c77f2479fc4c7526b6a514a71820b0d
Ronan Berntsen <ronanberntsen@gmail.com>
2024-08-24 21:35:54 +0200

    reset firefox to default (more or less, not really just no more shyfox)

--- home/mrhappy200/features/desktop/common/firefox.nix
+++ home/mrhappy200/features/desktop/common/firefox.nix
@@ -2,7 +2,7 @@
   programs.browserpass.enable = true;
 
 
-  #stylix.targets.firefox.enable = false;
+  stylix.targets.firefox.enable = false;
 
   programs.firefox = {
     enable = true;
@@ -12,7 +12,7 @@
       extensions = with pkgs.inputs.firefox-addons; [
         ublock-origin
         sponsorblock
-	userchrome-toggle-extended
+	#userchrome-toggle-extended
         dearrow
         browserpass
 	#sidebery
@@ -29,40 +29,12 @@
         "browser.shell.checkDefaultBrowser" = false;
         "browser.shell.defaultBrowserCheckCount" = 1;
         "browser.startup.homepage" = "https://search.nixos.org";
-        # "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
-				#"browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["_3c078156-979c-498b-8990-85f7987dd929_-browser-action","sponsorblocker_ajay_app-browser-action","dearrow_ajay_app-browser-action","userchrome-toggle_joolee_nl-browser-action","browserpass_maximbaz_com-browser-action","atbc_easonwong-browser-action"],"nav-bar":["back-button","stop-reload-button","forward-button","customizableui-special-spring1","urlbar-container","customizableui-special-spring2","zoom-controls","fxa-toolbar-menu-button","unified-extensions-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["sidebar-button","downloads-button","ublock0_raymondhill_net-browser-action","addon_darkreader_org-browser-action","customizableui-special-spring6","fullscreen-button","screenshot-button","history-panelmenu","bookmarks-menu-button","firefox-view-button","preferences-button","tabbrowser-tabs","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","browserpass_maximbaz_com-browser-action","userchrome-toggle_joolee_nl-browser-action","addon_darkreader_org-browser-action","atbc_easonwong-browser-action","dearrow_ajay_app-browser-action","sponsorblocker_ajay_app-browser-action","ublock0_raymondhill_net-browser-action","_3c078156-979c-498b-8990-85f7987dd929_-browser-action","developer-button"],"dirtyAreaCache":["unified-extensions-area","nav-bar","PersonalToolbar","TabsToolbar"],"currentVersion":20,"newElementCount":7}'';
         "dom.security.https_only_mode" = true;
         "identity.fxaccounts.enabled" = false;
         "privacy.trackingprotection.enabled" = true;
 	"privacy.sanitize.sanitizeOnShutdown" = true;
         "signon.rememberSignons" = false;
       };
-            extraConfig = ''
-      // userchrome.css usercontent.css activate
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-
-// Fill SVG Color
-user_pref("svg.context-properties.content.enabled", true);
-
-// CSS's `:has()` selector 
-user_pref("layout.css.has-selector.enabled", true);
-
-// Integrated calculator at urlbar
-user_pref("browser.urlbar.suggest.calculator", true);
-
-// Integrated unit convertor at urlbar
-user_pref("browser.urlbar.unitConversion.enabled", true);
-
-// Trim  URL
-user_pref("browser.urlbar.trimHttps", true);
-user_pref("browser.urlbar.trimURLs", true);
-
-// GTK rounded corners
-user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
-
-// Who is bogus? (fixes Sidebery tab dragging on Linux)
-user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
-'';
     };
   };
 
@@ -73,9 +45,6 @@ user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
       #"/nix/persist/home/mrhappy200/".directories = [ { directory = ".mozilla/firefox/mrhappy200/storage/default/";}];
     };
   };
-
-
-  home.file.".mozilla/firefox/${config.programs.firefox.profiles.mrhappy200.path}/chrome".source = "${inputs.ShyFox}/chrome";
 
   xdg.mimeApps.defaultApplications = {
     "text/html" = ["firefox.desktop"];
